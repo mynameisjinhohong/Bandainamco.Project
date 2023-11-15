262429c40c8af98d7e40841039f787fa2350c9f1
JinHo Hong <ghddhksduq@naver.com>
2023-11-15 14:21:40 +0900

    게임 오버시 이미지 뜨게

---
+++ BandaiNamCoProject/Assets/HJH/Script/GameOver_HJH.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOver_HJH : MonoBehaviour
+{
+    public GameObject bg;
+    public GameObject wave;
+    public GameObject clock;
+    public GameObject mushroom;
+    public GameObject cloud;
+    public GameObject eye;
+    public GameObject star;
+    public GameObject fish;
+    public GameObject lotus;
+    public GameObject rabbit;
+    public GameObject[] train;
+    public float fadeSpeed;
+    public void GameOver()
+    {
+        bg.SetActive(true);
+        StartCoroutine(FadeIn(bg));
+        for(int i = 0; i< ItemManager_LJH.Instance.items.Length; i++)
+        {
+            if (ItemManager_LJH.Instance.items[i].isVisited)
+            {
+                switch (ItemManager_LJH.Instance.items[i].itemType)
+                {
+                    case ItemType.Wave:
+                        wave.SetActive(true);
+                        StartCoroutine(FadeIn(wave));
+                        break;
+                    case ItemType.Clock:
+                        clock.SetActive(true);
+                        StartCoroutine(FadeIn(clock));
+                        break;
+                    case ItemType.Mushroom:
+                        mushroom.SetActive(true);
+                        StartCoroutine(FadeIn(mushroom));
+                        break;
+                    case ItemType.Rabbit:
+                        rabbit.SetActive(true);
+                        StartCoroutine(FadeIn(rabbit));
+                        break;
+                    case ItemType.Lotus:
+                        lotus.SetActive(true);
+                        StartCoroutine(FadeIn(lotus));
+                        break;
+                    case ItemType.Cloud:
+                        cloud.SetActive(true);
+                        StartCoroutine(FadeIn(cloud));
+                        break;
+                    case ItemType.Eye:
+                        eye.SetActive(true);
+                        StartCoroutine(FadeIn(eye));
+                        break;
+                    case ItemType.Fish:
+                        fish.SetActive(true);
+                        StartCoroutine(FadeIn(fish));
+                        break;
+                    case ItemType.Star:
+                        star.SetActive(true);
+                        StartCoroutine(FadeIn(star));
+                        break;
+                    case ItemType.Train:
+                        for(int j = 0; j <train.Length; j++)
+                        {
+                            train[j].SetActive(true);
+                            StartCoroutine(FadeIn(train[j]));
+                        }
+                        break;
+                }
+            }
+        }
+    }
+    IEnumerator FadeIn(GameObject img)
+    {
+        Image image = img.GetComponent<Image>();
+        float alpha = 0;
+        Color color = image.color;
+        color.a = 0;
+        image.color = color;
+        while (alpha < 1f)
+        {
+            alpha += 0.001f;
+            yield return new WaitForSecondsRealtime(fadeSpeed);
+            color.a = alpha;
+            image.color = color;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
