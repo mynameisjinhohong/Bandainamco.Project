bd3a995a591b6cba2f20a3fa7a1c9ef732468249
이진호 <55572533+JinHoLeeee@users.noreply.github.com>
2023-11-27 14:14:27 +0900

    Lotus 효과음 추가 (영역 enter/exit 시 서서히 음량 변화)

--- BandaiNamCoProject/Assets/LJH/Scripts/LotusBackground_LJH.cs
+++ BandaiNamCoProject/Assets/LJH/Scripts/LotusBackground_LJH.cs
@@ -1,3 +1,4 @@
+using Cysharp.Threading.Tasks;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,6 +10,11 @@ public class LotusBackground_LJH : MonoBehaviour
     [SerializeField] private int numOfPetalsToSpawn;
     [SerializeField] private float petalSpeed;
     [SerializeField] private float petalDisappearTime;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private float audioFadeTime;
+    [SerializeField] private bool customMaxVolume;
+    [Range(0f,1f)]
+    [SerializeField] private float maxVolume;
 
     private LotusPetalController controller;
 
@@ -24,12 +30,38 @@ public class LotusBackground_LJH : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag(TagStrings.PlayerTag))
+        {
+            SetAudio(true);
             controller.StartPetal();
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag(TagStrings.PlayerTag))
+        {
+            SetAudio(false);
             controller.StopPetal();
+        }
+    }
+
+    private async void SetAudio(bool isActive)
+    {
+        float currVolume = audioSource.volume;
+        float myMaxVolume = customMaxVolume == true ? maxVolume : 1.0f;
+        float destVolume = isActive == true ? myMaxVolume : 0.0f;
+        float elapsedTime = 0f;
+
+        if(isActive) audioSource.Play();
+
+        while(elapsedTime < audioFadeTime)
+        {
+            elapsedTime += Time.deltaTime;
+            float tempVolume = Mathf.Lerp(currVolume, destVolume, elapsedTime / audioFadeTime);
+            audioSource.volume = tempVolume;
+            await UniTask.Yield();
+        }
+
+        if (!isActive) audioSource.Stop();
     }
 }
