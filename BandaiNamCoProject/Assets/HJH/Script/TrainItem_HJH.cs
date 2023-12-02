a18c72b43fe2a8ca9a67730d950d87cb90ded03e
JinHo Hong <ghddhksduq@naver.com>
2023-12-03 01:32:44 +0900

    빌드를 위한 수정들

--- BandaiNamCoProject/Assets/HJH/Script/TrainItem_HJH.cs
+++ BandaiNamCoProject/Assets/HJH/Script/TrainItem_HJH.cs
@@ -2,7 +2,6 @@ using Cysharp.Threading.Tasks;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class TrainItem_HJH : BaseItem_LJH
 {
